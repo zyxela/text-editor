a0bf2268fd831a3ffd701ddebd32db77296a7878
FARGO <fargo26sur@mail.ru>
2023-09-13 15:59:03 +0300

    now it can change bg color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     implementation("com.github.skydoves:colorpickerview:2.2.4")
-    implementation("androidx.palette:palette-ktx:1.0.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.palette:palette-ktx:1.0.0")
 }
